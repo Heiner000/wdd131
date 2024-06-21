31714787ebd01e8b8b24ad40c2552e6daf2c776b
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2024-06-20 20:48:19 -0700

    updates bom function

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,8 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-// Create a li element that will hold each entries chapter title and an associated delete button.
 const item = document.createElement("li");
 
-// Create a delete button.
 const deleteBtn = document.createElement("button");
 deleteBtn.textContent = "❌";
 
