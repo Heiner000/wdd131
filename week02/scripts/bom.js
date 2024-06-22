dd7909f020fe36036d0c720f90cba2eea5685a78
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2024-06-21 19:22:09 -0700

    starts bom.js, doesn't work

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,12 +2,29 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const item = document.createElement("li");
+button.addEventListener("click", () => {
+    // check if input is blank
+    if (input.value.trim() !== "") {
+        // do more
+        const item = document.createElement("li");
 
-const deleteBtn = document.createElement("button");
-deleteBtn.textContent = "❌";
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
 
-item.textContent = input.value;
-item.append(deleteBtn);
+        item.textContent = input.value;
+        item.append(deleteBtn);
 
-list.append(item);
+        list.append(item);
+
+        deleteBtn.addEventListener("click", () => {
+            list.removeChild(item);
+            input.focus();
+        });
+        input.value = "";
+        input.focus();
+    } else {
+        console.log("error with something");
+        input.focus();
+    }
+    input.focus();
+});
